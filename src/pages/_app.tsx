3691721d40cd1edd3de61b174753daef10fa311f
Kazuki Sawada <kazuki@6715.jp>
2021-01-03 09:01:26 +0900

    canonical URL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,22 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "../styles/_global.scss";
 
-const App = ({ Component, pageProps }: AppProps) => (
-	<React.Fragment>
-		<Head>
-			<meta charSet="utf-8" />
-			<meta name="format-detection" content="telephone=no" />
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-		</Head>
-		<Header />
-		<Component {...pageProps} />
-		<Footer />
-	</React.Fragment>
-);
+const App = ({ Component, pageProps, router }: AppProps) => {
+	const canonicalUrl = `${process.env.NEXT_PUBLIC_BLOG_URL}${router.asPath}`;
+
+	return (
+		<React.Fragment>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta name="format-detection" content="telephone=no" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link rel="icon" href={process.env.NEXT_PUBLIC_AUTHOR_IMAGE} />
+				<link rel="canonical" href={canonicalUrl} />
+			</Head>
+			<Header />
+			<Component {...pageProps} />
+			<Footer />
+		</React.Fragment>
+	);
+};
 export default App;
