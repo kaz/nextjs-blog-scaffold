cae8e3214cc6a8d7780979490129bf3e37f4ebd0
Kazuki Sawada <kazuki@6715.jp>
2021-01-03 11:37:25 +0900

    update author's profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../styles/footer.module.scss";
+import { FacebookIcon, GithubIcon, TwitterIcon } from "./Icon";
 
 const Footer = () => {
 	return (
@@ -8,7 +9,25 @@ const Footer = () => {
 				<img src={process.env.NEXT_PUBLIC_AUTHOR_IMAGE} />
 				<div>
 					<small>Author</small>
-					<h3>{process.env.NEXT_PUBLIC_AUTHOR_NAME}</h3>
+					<h3>
+						{process.env.NEXT_PUBLIC_AUTHOR_NAME}
+						{process.env.NEXT_PUBLIC_AUTHOR_GITHUB_URL && (
+							<a target="_blank" href={process.env.NEXT_PUBLIC_AUTHOR_GITHUB_URL}>
+								<GithubIcon />
+							</a>
+						)}
+						{process.env.NEXT_PUBLIC_AUTHOR_TWITTER_URL && (
+							<a target="_blank" href={process.env.NEXT_PUBLIC_AUTHOR_TWITTER_URL}>
+								<TwitterIcon />
+							</a>
+						)}
+						{process.env.NEXT_PUBLIC_AUTHOR_FACEBOOK_URL && (
+							<a target="_blank" href={process.env.NEXT_PUBLIC_AUTHOR_FACEBOOK_URL}>
+								<FacebookIcon />
+							</a>
+						)}
+					</h3>
+					<p>{process.env.NEXT_PUBLIC_AUTHOR_BIO}</p>
 				</div>
 			</div>
 			{process.env.NEXT_PUBLIC_COPYRIGHT && (
