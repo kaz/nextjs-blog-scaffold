ba744de9069f125e94c3bf05baf4c0c20511eb9f
Kazuki Sawada <kazuki@6715.jp>
2021-01-03 11:22:39 +0900

    Icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import simpleIcons from "simple-icons";
+
+type Props = {
+	inverted?: boolean;
+};
+
+const icon = (name: string) => (props: Props) => {
+	const { svg, hex } = simpleIcons.get(name);
+	return (
+		<span
+			style={{
+				fill: props.inverted ? "white" : `#${hex}`,
+				backgroundColor: props.inverted ? `#${hex}` : "initial",
+			}}
+			dangerouslySetInnerHTML={{ __html: svg }}
+		/>
+	);
+};
+
+export const LineIcon = icon("line");
+export const GithubIcon = icon("github");
+export const TwitterIcon = icon("twitter");
+export const FacebookIcon = icon("facebook");
+export const HatenabookmarkIcon = icon("hatenabookmark");
--- src/components/SocialShare.tsx
+++ src/components/SocialShare.tsx
@@ -1,16 +1,11 @@
 import React, { useContext } from "react";
-import simpleIcons from "simple-icons";
 import styles from "../styles/socialshare.module.scss";
 import CanonicalUrlContext from "./CanonicalUrlContext";
+import { FacebookIcon, HatenabookmarkIcon, LineIcon, TwitterIcon } from "./Icon";
 
 type Props = {
 	pageTitle: string;
 };
-
-const lineIcon = simpleIcons.get("line");
-const twitterIcon = simpleIcons.get("twitter");
-const facebookIcon = simpleIcons.get("facebook");
-const hatenabookmarkIcon = simpleIcons.get("hatenabookmark");
 
 const SocialShare = ({ pageTitle }: Props) => {
 	const url = encodeURIComponent(useContext(CanonicalUrlContext));
@@ -18,30 +13,18 @@ const SocialShare = ({ pageTitle }: Props) => {
 
 	return (
 		<div className={styles.social}>
-			<a
-				target="__blank"
-				href={`https://social-plugins.line.me/lineit/share?url=${url}`}
-				style={{ backgroundColor: `#${lineIcon.hex}` }}
-				dangerouslySetInnerHTML={{ __html: lineIcon.svg }}
-			/>
-			<a
-				target="__blank"
-				href={`http://twitter.com/share?url=${url}&text=${text}`}
-				style={{ backgroundColor: `#${twitterIcon.hex}` }}
-				dangerouslySetInnerHTML={{ __html: twitterIcon.svg }}
-			/>
-			<a
-				target="__blank"
-				href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
-				style={{ backgroundColor: `#${facebookIcon.hex}` }}
-				dangerouslySetInnerHTML={{ __html: facebookIcon.svg }}
-			/>
-			<a
-				target="__blank"
-				href={`http://b.hatena.ne.jp/add?url=${url}`}
-				style={{ backgroundColor: `#${hatenabookmarkIcon.hex}` }}
-				dangerouslySetInnerHTML={{ __html: hatenabookmarkIcon.svg }}
-			/>
+			<a target="_blank" href={`https://social-plugins.line.me/lineit/share?url=${url}`}>
+				<LineIcon inverted={true} />
+			</a>
+			<a target="_blank" href={`http://twitter.com/share?url=${url}&text=${text}`}>
+				<TwitterIcon inverted={true} />
+			</a>
+			<a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}>
+				<FacebookIcon inverted={true} />
+			</a>
+			<a target="_blank" href={`http://b.hatena.ne.jp/add?url=${url}`}>
+				<HatenabookmarkIcon inverted={true} />
+			</a>
 		</div>
 	);
 };
