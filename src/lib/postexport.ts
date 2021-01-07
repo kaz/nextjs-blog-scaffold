461eb49fa1bc5653271408a7c11fd16b0bcdcbcc
Kazuki Sawada <kazuki@6715.jp>
2021-01-07 20:04:12 +0900

    fix: postexport fail

--- src/lib/postexport.ts
+++ src/lib/postexport.ts
@@ -1,8 +1,11 @@
+import { config } from "dotenv";
 import { promises as fs } from "fs";
 import path from "path";
 import { getContent as getSitemapContent } from "../pages/sitemap.xml";
 
 const ourDir = "./out";
+config({ path: ".env.local" });
+
 const write = (fileName: string) => async (content: Parameters<typeof fs.writeFile>[1]) => {
 	const dest = path.join(ourDir, fileName);
 	await fs.rmdir(dest, { recursive: true });
