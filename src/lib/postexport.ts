62ceec04eaf7d1457ef987ed125975c21e0f4adf
Kazuki Sawada <kazuki@6715.jp>
2021-01-07 21:49:18 +0900

    export 404.html

--- src/lib/postexport.ts
+++ src/lib/postexport.ts
@@ -8,8 +8,19 @@ config({ path: ".env.local" });
 
 const write = (fileName: string) => async (content: Parameters<typeof fs.writeFile>[1]) => {
 	const dest = path.join(ourDir, fileName);
+
 	await fs.rmdir(dest, { recursive: true });
 	return fs.writeFile(dest, content);
 };
 
-Promise.all([getSitemapContent().then(write("sitemap.xml"))]).catch(e => console.trace(e));
+const htmlize = async (dirName: string) => {
+	const dirPath = path.join(ourDir, dirName);
+	const src = path.join(dirPath, "index.html");
+	const dest = path.join(ourDir, `${dirName}.html`);
+
+	await fs.copyFile(src, dest);
+	return fs.rmdir(dirPath, { recursive: true });
+};
+
+const jobs = [htmlize("404"), getSitemapContent().then(write("sitemap.xml"))];
+Promise.all(jobs).catch(e => console.trace(e));
